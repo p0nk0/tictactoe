4e0b0b3647204db96982ad596aeef9556683a1b1
enoumy <enoumy@gmail.com>
2023-06-26 08:28:39 -0400

    Documented ./run-game-server.sh

--- run-game-server.sh
+++ run-game-server.sh
@@ -2,6 +2,9 @@
 
 # This script will run your game server locally. For help do
 # [$ ./run-game-server.sh --help]
+#
+# It will also build the executables for you, if they are not
+# present.
 
 SERVER_EXE="_build/default/server/bin/main.exe"
 CLIENT_JS="_build/default/client/bin/main.bc.js"
